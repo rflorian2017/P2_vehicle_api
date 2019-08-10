7c951376d4bcb72f5ce5f1726df008d5550c9ec4
rflorian2017 <roby_florian@yahoo.com>
2019-08-10 11:50:48 +0300

    correction for tests

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,9 +1,13 @@
 package com.udacity.pricing;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.udacity.pricing.service.PriceException;
+import com.udacity.pricing.service.PricingService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +17,9 @@ public class PricingServiceApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+    public void getPrice() throws PriceException {
+        // Test Price Service returns price of a given Vehicle
+        Assert.assertNotNull(PricingService.getPrice(1L));
+    }
 }
